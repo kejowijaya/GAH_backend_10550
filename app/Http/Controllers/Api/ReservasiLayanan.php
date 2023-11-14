609eb5b556898f0f5dade3631e5823193b5a8d2a
kejowijaya <200710550@students.uajy.ac.id>
2023-11-15 00:45:59 +0700

    fix reservasi layanan

--- app/Http/Controllers/Api/ReservasiLayanan.php
+++ app/Http/Controllers/Api/ReservasiLayanan.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 use Validator;
 use App\Models\Reservasi_Layanan;
+use App\Models\Fasilitas;
 
 class ReservasiLayanan extends Controller
 {
@@ -14,10 +15,7 @@ class ReservasiLayanan extends Controller
         $storeData = $request->all();
         $validate = Validator::make($storeData, [
             'id_reservasi' => 'required',
-            'id_fasilitas' => 'required',
-            'tanggal_pakai' => 'required|date',
-            'total_harga' => 'required',
-            'jumlah' => 'required',
+            'fasilitas' => 'required|array',
         ]);
 
         if($validate->fails())
@@ -25,6 +23,24 @@ class ReservasiLayanan extends Controller
 
         $reservasi_layanan = Reservasi_Layanan::create($storeData);
 
+        $totalHarga = 0;
+
+        foreach ($storeData['fasilitas'] as $fasilitas) {
+            $hargaFasilitas = Fasilitas::find($fasilitas['id_fasilitas'])->harga;
+        
+            $reservasi_layanan = new Reservasi_Layanan();
+            $reservasi_layanan->id_reservasi = $reservasi->id_reservasi;
+            $reservasi_layanan->id_fasilitas = $fasilitas['id_fasilitas'];
+            $reservasi_layanan->jumlah = $fasilitas['jumlah'];
+        
+            $subtotalItem = $fasilitas['jumlah'] * $hargaFasilitas;
+        
+            $totalHarga += $subtotalItem;
+        
+            $reservasi_layanan->total_harga = $subtotalItem;
+            $reservasi_layanan->save();
+        }
+        
         return response([
             'message' => 'Add Reservasi Layanan Success',
             'data' => $reservasi_layanan
--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\Api\ReservasiLayanan;
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
+| Here is where you can register API routes for your applrication. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
